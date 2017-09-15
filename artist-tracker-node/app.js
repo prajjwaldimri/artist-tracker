30a7078da6e7c0bf4de9068a5f48d37e3ef762ae
Prajjwal <prajjwaldimri@hotmail.com>
2017-09-15 22:59:18 +0530

    Added PassportJS Authentication to website with MLab MongoDB Server Integration

--- artist-tracker-node/app.js
+++ artist-tracker-node/app.js
@@ -3,23 +3,54 @@ const compression = require('compression');
 const express = require('express');
 const app = express();
 const hbs = require('hbs');
-var routes = require('./routes').router;
+const mongoose = require('mongoose');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const flash = require('connect-flash');
+const session = require('express-session');
 
-// TODO: Make this server compatible
-const PORT = 3000;
+const helpers = require('./helpers');
+const configDB = require('./config/database');
+
+var PORT = process.env.PORT || 3000;
+
+// Connect to the database
+mongoose.connect(configDB.url);
+
+require('./config/passport')(passport);
 
 // G-zip compression
 app.use(compression());
+
+// Reads cookieParser
+app.use(cookieParser());
+
+// Gets info from HTML forms
+app.use(bodyParser());
 
 // Set the templating engine to http://handlebarsjs.com/
 app.set('view engine', 'hbs');
 hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
+// Passport settings
+app.use(session({ secret: 'Why So Serious?' }));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Serve static JS and css files
 app.use(express.static(path.join(__dirname, 'dist')));
 
+// Pass variables to all our templates
+app.use((req, res, next) => {
+  res.locals.h = helpers;
+  next();
+});
+
 // Delegate all routing responsibility to routes module.
-app.use('/', routes);
+
+require('./routes/index.js')(app, passport);
 
 // Up the App
 app.listen(PORT, function () {
--- artist-tracker-node/routes/index.js
+++ artist-tracker-node/routes/index.js
@@ -1,17 +1,50 @@
-var express = require('express');
-var router = express.Router();
+const artistController = require('../controllers/artistcontroller');
 
-router.get('/', (req, res) => {
-  res.render('index', { title: 'Handlebars Up', body: 'Hello World!' });
-});
+module.exports = function (app, passport) {
+  app.get('/', artistController.homepage);
 
-router.get('/login', (req, res) => {
-  res.render('login', { title: 'Login' });
-});
+  app.get('/login', (req, res) => {
+    res.render('login', { title: 'Login' });
+  });
 
-// Test function
-var add = function (a, b) {
-  return a + b;
+  app.post(
+    '/login',
+    passport.authenticate('local-login', {
+      successRedirect: '/profile',
+      failureRedirect: '/login',
+      failureFlash: true
+    })
+  );
+
+  app.get('/signup', (req, res) => {
+    res.render('signup', { title: 'Signup' });
+  });
+
+  app.post(
+    '/signup',
+    passport.authenticate('local-signup', {
+      successRedirect: '/profile',
+      failureRedirect: '/signup',
+      failureFlash: true
+    })
+  );
+
+  // Only logged in users can visit this page
+  app.get('/profile', isLoggedIn, (req, res) => {
+    res.render('profile', { title: 'Profile', user: req.user });
+  });
+
+  app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  // app middleware to make sure if a user is logged in
+  function isLoggedIn (req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+
+    res.redirect('/');
+  }
 };
-
-module.exports = { router, add };
