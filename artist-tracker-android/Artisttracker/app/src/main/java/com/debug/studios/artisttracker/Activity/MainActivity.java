eb82f4f319c7d266d18344b491a6fa3274701982
Ayush Pant <ayushpant@hotmail.com>
2017-09-16 22:23:23 +0530

    Created android project with fuel library

---
+++ artist-tracker-android/Artisttracker/app/src/main/java/com/debug/studios/artisttracker/Activity/MainActivity.java
@@ -0,0 +1,23 @@
+package com.debug.studios.artisttracker.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.debug.studios.artisttracker.R;
+import com.github.kittinunf.fuel.Fuel;
+import com.github.kittinunf.fuel.core.FuelError;
+import com.github.kittinunf.fuel.core.Handler;
+import com.github.kittinunf.fuel.core.Request;
+import com.github.kittinunf.fuel.core.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
